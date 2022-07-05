b3a899ed7afbaeedc6e804e1a64f26819e9f3530
nathaly.salamanca <nathalysalamanca@gmail.com>
2022-07-05 16:33:26 -0500

    Adicion de configuracion clientes config server

---
+++ pokedex-config-server/src/main/java/com/obwankenobi/pokedex/PokedexConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.obwankenobi.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class PokedexConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexConfigServerApplication.class, args);
+	}
+
+}
