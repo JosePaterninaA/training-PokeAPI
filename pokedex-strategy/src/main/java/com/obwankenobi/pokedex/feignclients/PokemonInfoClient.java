e1d65a1ba50a43862d74005af45e2134623065be
Jose Paternina <jose.paternina.a@gmail.com>
2022-07-07 10:59:41 -0500

    Agregada configuración circuit-breaker

--- pokedex-strategy/src/main/java/com/obwankenobi/pokedex/feignclients/PokemonInfoClient.java
+++ pokedex-strategy/src/main/java/com/obwankenobi/pokedex/feignclients/PokemonInfoClient.java
@@ -2,6 +2,7 @@ package com.obwankenobi.pokedex.feignclients;
 
 import com.obwankenobi.pokedex.feignclients.model.PokemonInfo;
 import com.obwankenobi.pokedex.model.StrategyRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient("${feign.clients.info.name}")
 public interface PokemonInfoClient {
 
+    @CircuitBreaker(name = "pokemon-info-client", fallbackMethod = "getPokemonInfoFallback")
     @PostMapping(value = "${feign.clients.info.resource}")
     PokemonInfo getPokemonInfo(@RequestBody StrategyRequest strategyRequest);
+
+    default PokemonInfo getPokemonInfoFallback(StrategyRequest strategyRequest, Throwable error){
+        return new PokemonInfo();
+    }
 }
--- pokedex-strategy/src/main/java/com/obwankenobi/pokedex/feignclients/model/PokemonInfo.java
+++ pokedex-strategy/src/main/java/com/obwankenobi/pokedex/feignclients/model/PokemonInfo.java
@@ -2,11 +2,13 @@ package com.obwankenobi.pokedex.feignclients.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PokemonInfo {
     private String id;
     private String name;
