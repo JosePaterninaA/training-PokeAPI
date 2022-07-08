08e01bff580528324d9b84f85ad53c2147b14ea9
Jose Paternina <jose.paternina.a@gmail.com>
2022-07-08 10:03:01 -0500

    Agregado circuit-break annotation al PokemonTableTypesClient

--- pokedex-strategy/src/main/java/com/obwankenobi/pokedex/feignclients/PokemonTableTypesClient.java
+++ pokedex-strategy/src/main/java/com/obwankenobi/pokedex/feignclients/PokemonTableTypesClient.java
@@ -2,12 +2,19 @@ package com.obwankenobi.pokedex.feignclients;
 
 import com.obwankenobi.pokedex.feignclients.model.PokemonTableTypes;
 import com.obwankenobi.pokedex.model.StrategyRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient("${feign.clients.type.name}")
 public interface PokemonTableTypesClient {
 
+    @CircuitBreaker(name = "pokemon-tabletypes-client", fallbackMethod = "getPokemonTableTypesFallback")
     @PostMapping(value = "${feign.clients.type.resource}")
     PokemonTableTypes getPokemonTableTypes(@RequestBody StrategyRequest type);
+
+    default PokemonTableTypes getPokemonTableTypesFallback(StrategyRequest strategyRequest, Throwable error){
+        return new PokemonTableTypes();
+    }
+
 }
